35577e08222eb11213fe03d0efaaa9422ebfa7d6
MATHEUS CHAVES ARAUJO <002919@CESED.LOCAL>
2023-08-16 16:58:21 -0300

    Product routes structure created and tested on postman

--- app.js
+++ app.js
@@ -16,11 +16,9 @@ require('./configs')(app);
 //routers
 const authRouther = require('./routes/authRoutes');
 const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productRoutes');
 
 //middlewares
-app.use('/api/v1/auth', authRouther);
-app.use('/api/v1/users', userRouter);
-
 app.use(errorHandlingMiddleware);
 
 //routes
@@ -32,6 +30,10 @@ app.get('/api/v1', (req, res) => {
     console.log(req.signedCookies);
     res.send('E-commerce-api');
 });
+
+app.use('/api/v1/auth', authRouther);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/products', productRouter);
 
 
 require('./middleware/not-found')(app);
