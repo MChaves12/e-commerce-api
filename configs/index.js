4e2a79854779df076264cd10c86cefcf36b8bebe
MATHEUS CHAVES ARAUJO <002919@CESED.LOCAL>
2023-08-21 12:03:20 -0300

    Express-fileupload packeged installed. UploadImage route implemented

--- configs/index.js
+++ configs/index.js
@@ -1,7 +1,9 @@
 const { json, urlencoded } = require('express');
+const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
 
 module.exports = (app) => {
     app.set('trust proxy', 1);
@@ -9,6 +11,9 @@ module.exports = (app) => {
     app.use(cors());
     app.use(morgan('tiny'));
     app.use(cookieParser(process.env.JWT_Secret));
+
+    app.use(express.static('./public'));
+    app.use(fileUpload());
     app.use(json());
     app.use(urlencoded({extended: false}));
 };
--- controllers/productController.js
+++ controllers/productController.js
@@ -52,7 +52,26 @@ const deleteProduct = async (req, res) => {
 };
 
 const uploadImage = async (req, res) => {
-    res.send('upload image');
+    if(!req.files) {
+        throw new CustomErrors.BadRequestError('No file uploaded');
+    }
+    
+    const productImage = req.files.image;
+
+    if(!productImage.mimetype.startsWith('image')){
+        throw new CustomErrors.BadRequestError('Please upload image');
+    }
+
+    const maxSize = 1024 * 1024;
+
+    if(productImage.size > maxSize){
+        throw new CustomErrors.BadRequestError('Please upload image smaller then 1MB');
+    }
+
+    const imagePath = path.join(__dirname, '../public/uploads/' + `${productImage.name}`);
+
+    await productImage.mv(imagePath);
+    res.status(StatusCodes.OK).json({image: `/uploads/${productImage.name}`});
 };
 
 module.exports = {
